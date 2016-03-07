9a6b9391bbcb8f6812b87a6cb0ff9a3525514053
lilkr <maintiendrai@gmail.com>
2016-03-07 10:23:11 +0800

    modify port

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'ACCESS_KEY': '1xjcEhOOsX4qK6LCbuScVtXiWWaRr_p8be-LLlRd',
     'SECRET_KEY': 'RdGat82aWZHgGIx8KdhjkQErcOgx_IImXYVt6JRs',
     'Bucket_Name': 'maintiendrai',
-    'Port': 19110,
+    'Port': 81,
     'Uptoken_Url': 'uptoken',
     'Domain': 'http://7xkjke.com1.z0.glb.clouddn.com/'
 };
